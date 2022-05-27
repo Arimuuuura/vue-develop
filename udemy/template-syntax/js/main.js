e57eb8ef3bfcd6c9cbd2d635b8b23204f5797166
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-05-27 23:47:13 +0900

    v-html ディレクティブ

--- udemy/template-syntax/js/main.js
+++ udemy/template-syntax/js/main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
 	data: () => ({
 		message1: 'Hello arimura',
-		message2: 'Hello arimura2'
+		message2: 'Hello arimura2',
+		message3: 'Hello <span style="color: red">arimura3</span>'
 	}),
 	methods: {
 		clickHandler: function(event) {
