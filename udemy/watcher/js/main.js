85a8c4d1cb22be9cd447d3a1045b833311934310
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-05-28 14:17:21 +0900

    算出プロパティと監視プロパティの比較

--- udemy/watcher/js/main.js
+++ udemy/watcher/js/main.js
@@ -2,8 +2,17 @@ const app = Vue.createApp({
 	data: () => ({
 		message: 'Hello arimura',
 		km: 0,
-		m: 0
+		m: 0,
+		firstName: '',
+		lastName: '',
+		watchedFullName: ''
 	}),
+	// watch よりシンプルに実装できる
+	computed: {
+		computedFullName: function (params) {
+			return `${this.firstName} ${this.lastName}`
+		}
+	},
 	watch: {
 		message: function(newValue, oldValue) {
 			console.log('new: %s, old: %s', newValue, oldValue);
@@ -15,6 +24,12 @@ const app = Vue.createApp({
 		m: function(value) {
 			this.m = value;
 			this.km = value / 1000;
+		},
+		firstName: function(value) {
+			this.watchedFullName = `${value} ${this.lastName}`;
+		},
+		lastName: function(value) {
+			this.watchedFullName = `${this.firstName} ${value}`;
 		}
 	}
 }).mount('#app')
