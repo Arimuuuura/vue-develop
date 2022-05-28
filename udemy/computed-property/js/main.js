ba3018008d202632fb699ea149489e29ca6ef3c3
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-05-28 12:15:23 +0900

    getter, setter

--- udemy/computed-property/js/main.js
+++ udemy/computed-property/js/main.js
@@ -1,10 +1,19 @@
 const app = Vue.createApp({
 	data: () => ({
-		message: 'Hello arimura'
+		message: 'Hello arimura',
+		basePrice: 100,
 	}),
 	computed: {
 		reversedMessage: function() {
 			return this.message.split('').reverse().join('')
+		},
+		taxIncludedPrice: {
+			get: function() {
+				return this.basePrice * 1.1;
+			},
+			set: function(value) {
+				this.basePrice = value / 1.1;
+			}
 		}
 	}
 }).mount('#app');
