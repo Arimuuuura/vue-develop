b23af4ce4917746a7995f9ab704cc4f39bd39936
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-05-29 16:46:01 +0900

    コンポーネントで動的な処理

--- udemy/components/js/main.js
+++ udemy/components/js/main.js
@@ -2,12 +2,25 @@ const helloComponent = {
 	template: '<p>Hello! from local</p>'
 }
 
+const buttonCounter = {
+	template: '<div><span>count: </span><button v-on:click="countUp">{{ count }}</button></div>',
+	data: () => ({
+		count: 0
+	}),
+	methods: {
+		countUp: function(e) {
+			this.count++
+		}
+	}
+}
+
 const app = Vue.createApp({
 	data: () => ({
 		
 	}),
 	components: {
-		'hello-component': helloComponent
+		'hello-component': helloComponent,
+		'button-counter': buttonCounter
 	}
 })
 
