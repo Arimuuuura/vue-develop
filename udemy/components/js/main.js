611643e4f4cc52d895367afe2cb393d18ffa9dd3
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-05-29 16:35:46 +0900

    ローカルコンポーネントの定義

--- udemy/components/js/main.js
+++ udemy/components/js/main.js
@@ -1,11 +1,18 @@
+const helloComponent = {
+	template: '<p>Hello! from local</p>'
+}
+
 const app = Vue.createApp({
 	data: () => ({
 		
-	})
+	}),
+	components: {
+		'hello-component': helloComponent
+	}
 })
 
 app.component('hello', {
-	template: '<p>Hello! from component</p>'
+	template: '<p>Hello! from global</p>'
 });
 
 app.mount('#app');
